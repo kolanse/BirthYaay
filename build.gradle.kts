b384364f241bb555f97ae53e5d06b4ae2d07ce7e
kolanse <samuel.ochuba@decagon.dev>
2021-04-25 23:16:18 +0100

    configured kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,9 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.0-alpha05"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21"
+
+        classpath("com.android.tools.build:gradle:${Versions.gradle}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -20,6 +21,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,16 @@
+object Versions {
+    //app level
+    const val gradle = "7.0.0-alpha05"
+    const val kotlin = "1.4.21"
+
+    //libs
+    val coreKtx = "1.2.0"
+    val appcompat = "1.3.0-alpha01"
+    val constraintLayout = "2.0.0-beta8"
+    val materialDesign = "1.3.0"
+
+    //test
+    val junit = "4.12"
+    val extJunit = "1.1.1"
+    val espresso = "3.2.0"
+}
