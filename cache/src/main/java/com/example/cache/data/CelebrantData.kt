8565e6fa1a0b85519e6b8a5bb0808eb1c9503782
kolanse <samuel.ochuba@decagon.dev>
2021-05-11 00:44:38 +0100

    finished cache test and functions

--- cache/src/main/java/com/example/cache/data/CelebrantData.kt
+++ cache/src/main/java/com/example/cache/data/CelebrantData.kt
@@ -1,5 +1,6 @@
 package com.example.cache.data
 
+import android.util.Log
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -20,8 +21,45 @@ data class CelebrantData(
         @ColumnInfo(name= "interests") val interests: List<String> = listOf(),
         @ColumnInfo(name ="gifts") val gifts: List<String> = listOf(),
         @ColumnInfo(name = "note") val note: String = ""
+) {
+    /**
+     * checks whether the current date is the celebrant's birthday
+     */
 
-        )
+    fun isTodayBirthday(currentDate: Date): Boolean {
+
+        return currentDate.day == date.day && currentDate.month == date.month
+    }
+
+
+    /**
+     * Returns the number of day remaining ..
+     *
+     */
+    fun daysRemaining(): Int {
+
+      var yearCurrent =   Calendar.getInstance(Locale.getDefault())
+      var dayYearCurrent = yearCurrent.get(Calendar.DAY_OF_YEAR)
+
+      var c = Calendar.getInstance(Locale.getDefault())
+
+        c.set(yearCurrent.get(Calendar.YEAR), date.month -1, date.day)
+
+
+      var dayYearCelebrant = c.get(Calendar.DAY_OF_YEAR)
+        return if (dayYearCelebrant > dayYearCurrent){
+
+            dayYearCelebrant - dayYearCurrent
+        } else {
+
+            (dayYearCelebrant - dayYearCurrent) + yearCurrent.getActualMaximum(Calendar.DAY_OF_YEAR)
+        }
+
+    }
+
+
+    override fun toString() = name!!
+}
 
 
   data class Date(
